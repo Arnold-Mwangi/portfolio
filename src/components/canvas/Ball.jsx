513c63d4a62a4cd6d48d83c8e94949a5d47bdbb8
Arnold-Mwangi <kirigwimwangi18@yahoo.com>
2023-11-28 15:11:49 +0300

    Added animation to technologies

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,57 +1,22 @@
-import React, { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
-import {
-  Decal,
-  Float,
-  OrbitControls,
-  Preload,
-  useTexture,
-} from "@react-three/drei";
+import React, { useState, useEffect } from "react";
+import "./RotatingIcons.css"; // Create a CSS file for styling
 
-import CanvasLoader from "../Loader";
+const RotatingIcon = ({ icon }) => {
+  const [rotation, setRotation] = useState(0);
 
-const Ball = (props) => {
-  const [decal] = useTexture([props.imgUrl]);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setRotation((prevRotation) => prevRotation + 1);
+    }, 16); // Adjust the interval as needed for the desired rotation speed
+
+    return () => clearInterval(intervalId);
+  }, []);
 
   return (
-    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-      <ambientLight intensity={0.25} />
-      <directionalLight position={[0, 0, 0.05]} />
-      <mesh castShadow receiveShadow scale={2.75}>
-        <icosahedronGeometry args={[1, 1]} />
-        <meshStandardMaterial
-          color='#fff8eb'
-          polygonOffset
-          polygonOffsetFactor={-5}
-          flatShading
-        />
-        <Decal
-          position={[0, 0, 1]}
-          rotation={[2 * Math.PI, 0, 6.25]}
-          scale={1}
-          map={decal}
-          flatShading
-        />
-      </mesh>
-    </Float>
+    <div className="rotating-icon" style={{ transform: `rotate(${rotation}deg)` }}>
+      <img src={icon} alt="Icon" />
+    </div>
   );
 };
 
-const BallCanvas = ({ icon }) => {
-  return (
-    <Canvas
-      frameloop='demand'
-      dpr={[1, 2]}
-      gl={{ preserveDrawingBuffer: true }}
-    >
-      <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls enableZoom={false} />
-        <Ball imgUrl={icon} />
-      </Suspense>
-
-      <Preload all />
-    </Canvas>
-  );
-};
-
-export default BallCanvas;
+export default RotatingIcon;
